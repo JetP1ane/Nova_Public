8f7972cb994e6081bcb87163561a937db608e7d5
Jetp1ane <jamesbarnett1128@gmail.com>
2025-01-22 21:16:37 -0700

    second

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Orbitron } from "next/font/google";
+import { Orbitron } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const orbitron = Orbitron({ subsets: ["latin"] });
 
@@ -26,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${orbitron.className} bg-black`}>
+      <body className={`${orbitron.className} bg-black`}>
         {children}
       </body>
     </html>
